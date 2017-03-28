7e067412210bc9a7a7b0d47e8ac8a1654fadb426
USER <user@USERde-iMac.local>
2017-03-28 16:03:14 +0800

    a

--- src/view.js
+++ src/view.js
@@ -41,7 +41,82 @@ $(document).ready(function(){
 		$(window).scroll();
 	});
 
-	$(window).scroll($.throttle(function(){
+	/**
+	  * 频率控制函数， fn执行次数不超过 1 次/delay
+	  * @param fn{Function}     传入的函数
+	  * @param delay{Number}    时间间隔
+	  * @param options{Object}  如果想忽略开始边界上的调用则传入 {leading:false},
+	  *                         如果想忽略结束边界上的调用则传入 {trailing:false},
+	  * @returns {Function}     返回调用函数
+	  */
+	_.throttle = function(func, wait, options) {
+	    var context, args, result;
+	    var timeout = null;
+	    var previous = 0;
+	    if (!options) options = {};
+	    var later = function() {
+	        previous = options.leading === false ? 0 : _.now();
+	        timeout = null;
+	        result = func.apply(context, args);
+	        if (!timeout) context = args = null;
+	    };
+	    return function() {
+	        var now = _.now();
+	        if (!previous && options.leading === false) previous = now;
+	        var remaining = wait - (now - previous);
+	        context = this;
+	        args = arguments;
+	        if (remaining <= 0 || remaining > wait) {
+	            clearTimeout(timeout);
+	            timeout = null;
+	            previous = now;
+	            result = func.apply(context, args);
+	            if (!timeout) context = args = null;
+	        } else if (!timeout && options.trailing !== false) {
+	            timeout = setTimeout(later, remaining);
+	        }
+	        return result;
+	    };
+	};
+	/**
+	 * 空闲控制函数， fn仅执行一次
+	 * @param fn{Function}     传入的函数
+	 * @param delay{Number}    时间间隔
+	 * @param options{Object}  如果想忽略开始边界上的调用则传入 {leading:false},
+	 *                         如果想忽略结束边界上的调用则传入 {trailing:false},
+	 * @returns {Function}     返回调用函数
+	 */
+	_.debounce = function(func, wait, immediate) {
+	    var timeout, args, context, timestamp, result;
+
+	    var later = function() {
+	        var last = _.now() - timestamp;
+	        if (last < wait && last > 0) {
+	            timeout = setTimeout(later, wait - last);
+	        } else {
+	            timeout = null;
+	            if (!immediate) {
+	                result = func.apply(context, args);
+	                if (!timeout) context = args = null;
+	            }
+	        }
+	    };
+
+	    return function() {
+	        context = this;
+	        args = arguments;
+	        timestamp = _.now();
+	        var callNow = immediate && !timeout;
+	        if (!timeout) timeout = setTimeout(later, wait);
+	        if (callNow) {
+	            result = func.apply(context, args);
+	            context = args = null;
+	        }
+	        return result;
+	    };
+	};
+
+	$(window).scroll(_.throttle(function(){
 		// var tur = true; 
 		// if(tur){ 
 		// 	setTimeout(function(){
