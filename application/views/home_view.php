8d77788da3edbace13e7308fc13c847a3bdf65a3
USER <user@USERde-iMac.local>
2017-04-01 14:43:19 +0800

    a

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -8,13 +8,10 @@
 	<?php require_once 'header_view.php' ?>
 	<div class="content">
 		<div class="main-container">
-			<?php
-			for($i=0;$i<3;$i++){
-			?>
 			<div class="category-container-wrap">
 				<div class="category-container">
 					<div class="category-title">
-						<h1>Title</h1>
+						<h1><b>地理</b> 分类下的最新文档</h1>
 					</div>
 					<div class="category">
 						<ul>
@@ -61,9 +58,156 @@
 				</div>
 				<div class="clearfix"></div>
 			</div>
-			<?php
-			}
-			?>
+			<div class="category-container-wrap">
+				<div class="category-container">
+					<div class="category-title">
+						<h1><b>历史</b> 分类下的最新文档</h1>
+					</div>
+					<div class="category">
+						<ul>
+							<?php
+							for($j=0;$j<5;$j++){
+							?>
+							<li>
+								<a href="<?=base_url('view/index/'.($j%2+1))?>"><h3>Can we get the Size of each page of Pdf using Php?</h3></a>
+							</li>
+							<?php
+							}
+							?>
+						</ul>
+						<ul>
+							<?php
+							for($j=0;$j<5;$j++){
+							?>
+							<li>
+								<a href="<?=base_url('view/index/'.($j%2+1))?>"><h3>Can we get the Size of each page of Pdf using Php?</h3></a>
+							</li>
+							<?php
+							}
+							?>
+						</ul>
+						<div class="clearfix"></div>
+					</div>
+				</div>
+				<div class="recommend-container">
+					<div class="relative-container">
+						<div class="relative-title">
+							<h1>Title</h1>
+						</div>
+						<ul>
+							<li>list</li>
+							<li>list</li>
+							<li>list</li>
+							<li>list</li>
+							<li>list</li>
+							<li>list</li>
+							<li>list</li>
+							<li>list</li>
+						</ul>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="category-container-wrap">
+				<div class="category-container">
+					<div class="category-title">
+						<h1><b>政治</b> 分类下的最新文档</h1>
+					</div>
+					<div class="category">
+						<ul>
+							<?php
+							for($j=0;$j<5;$j++){
+							?>
+							<li>
+								<a href="<?=base_url('view/index/'.($j%2+1))?>"><h3>Can we get the Size of each page of Pdf using Php?</h3></a>
+							</li>
+							<?php
+							}
+							?>
+						</ul>
+						<ul>
+							<?php
+							for($j=0;$j<5;$j++){
+							?>
+							<li>
+								<a href="<?=base_url('view/index/'.($j%2+1))?>"><h3>Can we get the Size of each page of Pdf using Php?</h3></a>
+							</li>
+							<?php
+							}
+							?>
+						</ul>
+						<div class="clearfix"></div>
+					</div>
+				</div>
+				<div class="recommend-container">
+					<div class="relative-container">
+						<div class="relative-title">
+							<h1>Title</h1>
+						</div>
+						<ul>
+							<li>list</li>
+							<li>list</li>
+							<li>list</li>
+							<li>list</li>
+							<li>list</li>
+							<li>list</li>
+							<li>list</li>
+							<li>list</li>
+						</ul>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="category-container-wrap">
+				<div class="category-container">
+					<div class="category-title">
+						<h1><b>文综</b> 分类下的最新文档</h1>
+					</div>
+					<div class="category">
+						<ul>
+							<?php
+							for($j=0;$j<5;$j++){
+							?>
+							<li>
+								<a href="<?=base_url('view/index/'.($j%2+1))?>"><h3>Can we get the Size of each page of Pdf using Php?</h3></a>
+							</li>
+							<?php
+							}
+							?>
+						</ul>
+						<ul>
+							<?php
+							for($j=0;$j<5;$j++){
+							?>
+							<li>
+								<a href="<?=base_url('view/index/'.($j%2+1))?>"><h3>Can we get the Size of each page of Pdf using Php?</h3></a>
+							</li>
+							<?php
+							}
+							?>
+						</ul>
+						<div class="clearfix"></div>
+					</div>
+				</div>
+				<div class="recommend-container">
+					<div class="relative-container">
+						<div class="relative-title">
+							<h1>Title</h1>
+						</div>
+						<ul>
+							<li>list</li>
+							<li>list</li>
+							<li>list</li>
+							<li>list</li>
+							<li>list</li>
+							<li>list</li>
+							<li>list</li>
+							<li>list</li>
+						</ul>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
 		</div>
 	</div>
 	<?php require_once 'footer_view.php' ?>
