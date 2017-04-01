468ea25595b555f150c1c57658a203a49f11cfdc
USER <user@USERde-iMac.local>
2017-04-01 15:43:47 +0800

    a

--- application/views/view_view.php
+++ application/views/view_view.php
@@ -80,8 +80,8 @@
 		<div class="main-container">
 			<div class="reader-tools-bar">
 				<div class="left">
-					<div class="page-prev">< 上一页</div>
-					<div class="page-next">下一页 ></div>
+					<a class="page-prev">< 上一页</a>
+					<a class="page-next">下一页 ></a>
 				</div>
 				<div class="center"></div>
 				<div class="right">
