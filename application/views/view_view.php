ef07128eb48c5fa0d7b7e4995575fd08883bfa03
miaojuanfeng <miaojuanfeng@qq.com>
2017-04-03 20:37:57 +0800

    k

--- application/views/view_view.php
+++ application/views/view_view.php
@@ -29,13 +29,13 @@
 				<?php
 				for($i=1;$i<=$page['num'];$i++){
 				?>
-					<div class="page" id="page_<?=$i?>" page="<?=$i?>" style="width: <?=$page['width']?>px;height: <?=$page['height']?>px; <?php if( $i == 1) echo 'border-top:none;'; ?>">
+					<div class="page" id="page_<?=$i?>" page="<?=$i?>" style="width: <?=$page['width']?>px;height: <?=$page['height']?>px; <?php if( $i == 1) echo 'border-top:none;margin-bottom:10px;'; ?>">
 						<div class="pv" id="pv_<?=$i?>" page="<?=$i?>"></div>
 					</div>
 				<?php
 					if( $i == 1 ){
 				?>
-					<div style="display: flex; border: 1px solid #eee; box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.2); margin-bottom: 15px;">
+					<div style="display: flex; border: 1px solid #eee; box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.2); margin-bottom: 10px;">
 						<img src="<?=base_url('assets/img/view_ad_2.jpg')?>" width="960" />
 					</div>
 				<?php
