ea635d6b9834ba13d2d3c1f1b1789d3469288c26
miaojuanfeng <miaojuanfeng@qq.com>
2017-04-03 21:55:29 +0800

    k

--- application/models/Home_model.php
+++ application/models/Home_model.php
@@ -15,13 +15,13 @@ class home_model extends CI_Model{
 
     public function get_new_doc(){
         $retval = array();
-    	$query = $this->db->query("SELECT doc_url, doc_title FROM m_doc WHERE doc_deleted = 0 AND doc_cate_id = 1 ORDER BY doc_id DESC");
+    	$query = $this->db->query("SELECT doc_url, doc_title FROM m_doc WHERE doc_deleted = 0 AND doc_cate_id = 1 ORDER BY doc_id DESC LIMIT 16");
     	$retval[1] = $query->result_array();
-        $query = $this->db->query("SELECT doc_url, doc_title FROM m_doc WHERE doc_deleted = 0 AND doc_cate_id = 2 ORDER BY doc_id DESC");
+        $query = $this->db->query("SELECT doc_url, doc_title FROM m_doc WHERE doc_deleted = 0 AND doc_cate_id = 2 ORDER BY doc_id DESC LIMIT 16");
         $retval[2] = $query->result_array();
-        $query = $this->db->query("SELECT doc_url, doc_title FROM m_doc WHERE doc_deleted = 0 AND doc_cate_id = 3 ORDER BY doc_id DESC");
+        $query = $this->db->query("SELECT doc_url, doc_title FROM m_doc WHERE doc_deleted = 0 AND doc_cate_id = 3 ORDER BY doc_id DESC LIMIT 16");
         $retval[3] = $query->result_array();
-        $query = $this->db->query("SELECT doc_url, doc_title FROM m_doc WHERE doc_deleted = 0 AND doc_cate_id = 4 ORDER BY doc_id DESC");
+        $query = $this->db->query("SELECT doc_url, doc_title FROM m_doc WHERE doc_deleted = 0 AND doc_cate_id = 4 ORDER BY doc_id DESC LIMIT 16");
         $retval[4] = $query->result_array();
         return $retval;
     }
