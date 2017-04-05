f1b8c9071fd275f6884acb1a7e8277aea1307c73
USER <user@USERde-iMac.local>
2017-04-05 18:59:29 +0800

    a

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -8,200 +8,202 @@
 	<?php require_once 'header_view.php' ?>
 	<div class="content">
 		<div class="main-container">
-			<div class="category-container">
-				<div class="category-title">
-					<h1><b>地理</b> 分类下的最新文档</h1>
-				</div>
-				<div class="category">
-					<ul>
-						<?php
-						foreach($doc['new'][1] as $key => $value){
-							if( !($key % 2) ){
-						?>
-						<li>
-							<a href="<?=base_url('view/'.($value['doc_url']))?>" target="_blank"><h3><?=$value['doc_title']?></h3></a>
-						</li>
-						<?php
+			<div>
+				<div class="category-container">
+					<div class="category-title">
+						<h1><b>地理</b> 分类下的最新文档</h1>
+					</div>
+					<div class="category">
+						<ul>
+							<?php
+							foreach($doc['new'][1] as $key => $value){
+								if( !($key % 2) ){
+							?>
+							<li>
+								<a href="<?=base_url('view/'.($value['doc_url']))?>" target="_blank"><h3><?=$value['doc_title']?></h3></a>
+							</li>
+							<?php
+								}
 							}
-						}
-						?>
-					</ul>
-					<ul>
-						<?php
-						foreach($doc['new'][1] as $key => $value){
-							if( ($key % 2) ){
-						?>
-						<li>
-							<a href="<?=base_url('view/'.($value['doc_url']))?>" target="_blank"><h3><?=$value['doc_title']?></h3></a>
-						</li>
-						<?php
+							?>
+						</ul>
+						<ul>
+							<?php
+							foreach($doc['new'][1] as $key => $value){
+								if( ($key % 2) ){
+							?>
+							<li>
+								<a href="<?=base_url('view/'.($value['doc_url']))?>" target="_blank"><h3><?=$value['doc_title']?></h3></a>
+							</li>
+							<?php
+								}
 							}
-						}
-						?>
-					</ul>
-					<div class="clearfix"></div>
+							?>
+						</ul>
+						<div class="clearfix"></div>
+					</div>
+					<div style="display: flex; border: 1px solid #eee; box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.2);">
+						<img src="<?=base_url('assets/img/view_ad_2.jpg')?>" width="960" />
+					</div>
 				</div>
-				<div style="display: flex; border: 1px solid #eee; box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.2);">
-					<img src="<?=base_url('assets/img/view_ad_2.jpg')?>" width="960" />
+				<div class="category-container">
+					<div class="category-title">
+						<h1><b>历史</b> 分类下的最新文档</h1>
+					</div>
+					<div class="category">
+						<ul>
+							<?php
+							foreach($doc['new'][2] as $key => $value){
+								if( !($key % 2) ){
+							?>
+							<li>
+								<a href="<?=base_url('view/'.($value['doc_url']))?>" target="_blank"><h3><?=$value['doc_title']?></h3></a>
+							</li>
+							<?php
+								}
+							}
+							?>
+						</ul>
+						<ul>
+							<?php
+							foreach($doc['new'][2] as $key => $value){
+								if( ($key % 2) ){
+							?>
+							<li>
+								<a href="<?=base_url('view/'.($value['doc_url']))?>" target="_blank"><h3><?=$value['doc_title']?></h3></a>
+							</li>
+							<?php
+								}
+							}
+							?>
+						</ul>
+						<div class="clearfix"></div>
+					</div>
+					<div style="display: flex; border: 1px solid #eee; box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.2);">
+						<img src="<?=base_url('assets/img/view_ad_2.jpg')?>" width="960" />
+					</div>
+				</div>
+				<div class="category-container">
+					<div class="category-title">
+						<h1><b>政治</b> 分类下的最新文档</h1>
+					</div>
+					<div class="category">
+						<ul>
+							<?php
+							foreach($doc['new'][3] as $key => $value){
+								if( !($key % 2) ){
+							?>
+							<li>
+								<a href="<?=base_url('view/'.($value['doc_url']))?>" target="_blank"><h3><?=$value['doc_title']?></h3></a>
+							</li>
+							<?php
+								}
+							}
+							?>
+						</ul>
+						<ul>
+							<?php
+							foreach($doc['new'][3] as $key => $value){
+								if( ($key % 2) ){
+							?>
+							<li>
+								<a href="<?=base_url('view/'.($value['doc_url']))?>" target="_blank"><h3><?=$value['doc_title']?></h3></a>
+							</li>
+							<?php
+								}
+							}
+							?>
+						</ul>
+						<div class="clearfix"></div>
+					</div>
+					<div style="display: flex; border: 1px solid #eee; box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.2);">
+						<img src="<?=base_url('assets/img/view_ad_2.jpg')?>" width="960" />
+					</div>
+				</div>
+				<div class="category-container">
+					<div class="category-title">
+						<h1><b>文综</b> 分类下的最新文档</h1>
+					</div>
+					<div class="category">
+						<ul>
+							<?php
+							foreach($doc['new'][4] as $key => $value){
+								if( !($key % 2) ){
+							?>
+							<li>
+								<a href="<?=base_url('view/'.($value['doc_url']))?>" target="_blank"><h3><?=$value['doc_title']?></h3></a>
+							</li>
+							<?php
+								}
+							}
+							?>
+						</ul>
+						<ul>
+							<?php
+							foreach($doc['new'][4] as $key => $value){
+								if( ($key % 2) ){
+							?>
+							<li>
+								<a href="<?=base_url('view/'.($value['doc_url']))?>" target="_blank"><h3><?=$value['doc_title']?></h3></a>
+							</li>
+							<?php
+								}
+							}
+							?>
+						</ul>
+						<div class="clearfix"></div>
+					</div>
+					<div style="display: flex; border: 1px solid #eee; box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.2);">
+						<img src="<?=base_url('assets/img/view_ad_2.jpg')?>" width="960" />
+					</div>
 				</div>
 			</div>
-			<div class="category-container">
-				<div class="category-title">
-					<h1><b>历史</b> 分类下的最新文档</h1>
-				</div>
-				<div class="category">
+			<div class="recommend-container">
+				<div class="relative-container">
+					<div class="relative-title">
+						<h1>推荐的文档</h1>
+					</div>
 					<ul>
 						<?php
-						foreach($doc['new'][2] as $key => $value){
-							if( !($key % 2) ){
+						foreach ($doc['hot'][1] as $key => $value) {
 						?>
-						<li>
-							<a href="<?=base_url('view/'.($value['doc_url']))?>" target="_blank"><h3><?=$value['doc_title']?></h3></a>
-						</li>
+						<li><a href="<?=base_url('view/'.$value['doc_url']);?>" target="_blank"><?=$value['doc_title']?></a></li>
 						<?php
-							}
 						}
 						?>
 					</ul>
+				</div>
+				<div class="relative-container">
+					<div class="relative-title">
+						<h1>推荐的文档</h1>
+					</div>
 					<ul>
 						<?php
-						foreach($doc['new'][2] as $key => $value){
-							if( ($key % 2) ){
+						foreach ($doc['hot'][2] as $key => $value) {
 						?>
-						<li>
-							<a href="<?=base_url('view/'.($value['doc_url']))?>" target="_blank"><h3><?=$value['doc_title']?></h3></a>
-						</li>
+						<li><a href="<?=base_url('view/'.$value['doc_url']);?>" target="_blank"><?=$value['doc_title']?></a></li>
 						<?php
-							}
 						}
 						?>
 					</ul>
-					<div class="clearfix"></div>
 				</div>
-				<div style="display: flex; border: 1px solid #eee; box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.2);">
-					<img src="<?=base_url('assets/img/view_ad_2.jpg')?>" width="960" />
+				<div class="relative-container">
+					<div class="relative-title">
+						<h1>推荐的文档</h1>
+					</div>
+					<ul>
+						<?php
+						foreach ($doc['hot'][4] as $key => $value) {
+						?>
+						<li><a href="<?=base_url('view/'.$value['doc_url']);?>" target="_blank"><?=$value['doc_title']?></a></li>
+						<?php
+						}
+						?>
+					</ul>
 				</div>
 			</div>
-			<div class="category-container">
-				<div class="category-title">
-					<h1><b>政治</b> 分类下的最新文档</h1>
-				</div>
-				<div class="category">
-					<ul>
-						<?php
-						foreach($doc['new'][3] as $key => $value){
-							if( !($key % 2) ){
-						?>
-						<li>
-							<a href="<?=base_url('view/'.($value['doc_url']))?>" target="_blank"><h3><?=$value['doc_title']?></h3></a>
-						</li>
-						<?php
-							}
-						}
-						?>
-					</ul>
-					<ul>
-						<?php
-						foreach($doc['new'][3] as $key => $value){
-							if( ($key % 2) ){
-						?>
-						<li>
-							<a href="<?=base_url('view/'.($value['doc_url']))?>" target="_blank"><h3><?=$value['doc_title']?></h3></a>
-						</li>
-						<?php
-							}
-						}
-						?>
-					</ul>
-					<div class="clearfix"></div>
-				</div>
-				<div style="display: flex; border: 1px solid #eee; box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.2);">
-					<img src="<?=base_url('assets/img/view_ad_2.jpg')?>" width="960" />
-				</div>
-			</div>
-			<div class="category-container">
-				<div class="category-title">
-					<h1><b>文综</b> 分类下的最新文档</h1>
-				</div>
-				<div class="category">
-					<ul>
-						<?php
-						foreach($doc['new'][4] as $key => $value){
-							if( !($key % 2) ){
-						?>
-						<li>
-							<a href="<?=base_url('view/'.($value['doc_url']))?>" target="_blank"><h3><?=$value['doc_title']?></h3></a>
-						</li>
-						<?php
-							}
-						}
-						?>
-					</ul>
-					<ul>
-						<?php
-						foreach($doc['new'][4] as $key => $value){
-							if( ($key % 2) ){
-						?>
-						<li>
-							<a href="<?=base_url('view/'.($value['doc_url']))?>" target="_blank"><h3><?=$value['doc_title']?></h3></a>
-						</li>
-						<?php
-							}
-						}
-						?>
-					</ul>
-					<div class="clearfix"></div>
-				</div>
-				<div style="display: flex; border: 1px solid #eee; box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.2);">
-					<img src="<?=base_url('assets/img/view_ad_2.jpg')?>" width="960" />
-				</div>
-			</div>
+			<div class="clearfix"></div>
 		</div>
-		<div class="recommend-container">
-			<div class="relative-container">
-				<div class="relative-title">
-					<h1>推荐的文档</h1>
-				</div>
-				<ul>
-					<?php
-					foreach ($doc['hot'][1] as $key => $value) {
-					?>
-					<li><a href="<?=base_url('view/'.$value['doc_url']);?>" target="_blank"><?=$value['doc_title']?></a></li>
-					<?php
-					}
-					?>
-				</ul>
-			</div>
-			<div class="relative-container">
-				<div class="relative-title">
-					<h1>推荐的文档</h1>
-				</div>
-				<ul>
-					<?php
-					foreach ($doc['hot'][2] as $key => $value) {
-					?>
-					<li><a href="<?=base_url('view/'.$value['doc_url']);?>" target="_blank"><?=$value['doc_title']?></a></li>
-					<?php
-					}
-					?>
-				</ul>
-			</div>
-			<div class="relative-container">
-				<div class="relative-title">
-					<h1>推荐的文档</h1>
-				</div>
-				<ul>
-					<?php
-					foreach ($doc['hot'][4] as $key => $value) {
-					?>
-					<li><a href="<?=base_url('view/'.$value['doc_url']);?>" target="_blank"><?=$value['doc_title']?></a></li>
-					<?php
-					}
-					?>
-				</ul>
-			</div>
-		</div>
-		<div class="clearfix"></div>
 	</div>
 	<?php require_once 'footer_view.php' ?>
 </body>
