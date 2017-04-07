71341c78977ddad29e523fc58e5f06583d7afdc0
USER <user@USERde-iMac.local>
2017-04-07 19:52:29 +0800

    a

---
+++ application/views/user_verify_view.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>缪缪文科 - 专业的文科文档分享平台</title>
+	<?php require_once 'meta_view.php' ?>
+</head>
+<body>
+	<?php require_once 'header_view.php' ?>
+	<div class="content">
+		<div class="main-container">
+			<div class="verify-container">
+				<h3 class="verify-title">已向您的电子邮箱中发送了一封验证邮件，请登录邮箱激活账号。</h3>
+				<p class="verify-title2">未收到邮件？温馨提示：</p>
+				<ul>
+					<li>注册的邮箱地址是否正确。</li>
+					<li>邮件是否被放入到了垃圾箱。</li>
+					<li>邮件链接有效期XXX，过时失效</li>
+					<li>如有任何意见或建议，请及时反馈给我们。</li>
+				</ul>
+				<p class="verify-title2">还是未收到邮件？</p>
+				<ul>
+					<li><a href="javascript:;">重新发送</a></li>
+				</ul>
+			</div>
+			<div class="recommend-container">
+
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+	<?php require_once 'footer_view.php' ?>
+</body>
+</html>
