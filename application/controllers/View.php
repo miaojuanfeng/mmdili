802f1360bdf92bb2c2a3c7a12191234d2e277c01
USER <user@USERde-iMac.local>
2017-04-07 16:53:12 +0800

    a

--- application/controllers/View.php
+++ application/controllers/View.php
@@ -32,7 +32,7 @@ class View extends CI_Controller {
 	public function index($view_id)
 	{
 		var_dump($view_id);
-		if( !is_numeric($view_id) ) ){
+		if( !is_numeric($view_id) ){
 			redirect(base_url('error'.'.html'));
 		}
 		$data['class_footer'] = 'view-footer';
