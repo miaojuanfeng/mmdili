abd12a6d8676f8a1f0cf17e43a0eeaa3d38c8006
USER <user@USERde-iMac.local>
2017-04-21 11:31:53 +0800

    a

--- application/views/page/code_view.php
+++ application/views/page/code_view.php
@@ -10,12 +10,14 @@
 		<div class="main-container">
 			<div class="page-container">
 				<h3 class="page-title">技术架构</h3>
-				<p class="page-title2">核心技术：</p>
+				<p class="page-title2">CII 扩展框架：</p>
 				<ul>
-					<li>缪缪文科 采用 M.J.F 研发的 CII 高性能扩展框架，全称为 CodeIgniter Internal Framework 。
-					这是一个现时非常流行的开源框架 CodeIgniter 以PHP扩展的方式实现。
-					完全采用C语言编写，与ZEND API协作，编译成动态链接库在PHP启动时载入运行，采用CII框架开发网站的速度和便利不输于 CodeIgniter。
-					经测试，运行在CII框架上的网站的响应速度比采用CodeIgniter框架的网站快了近10倍。</li>
+					<li>本站运行在 M.J.F 研发的 CII 高性能扩展框架中，全称为 CodeIgniter Internal Framework 。</li>
+					<li>这是根据目前非常流行的开源框架 CodeIgniter 的架构，以 PHP 扩展方式实现的框架。</li>
+					<li>完全采用 C 语言编写而成，编译为动态链接库，在 PHP 启动时装载运行。</li>
+					<li>经过线下单元测试与线上长期运行，CII 框架运行时不会产生 Segment Fault 与 Memory Leak 。</li>
+					<li>采用 CII 框架开发网站的速度和便利不输于 CodeIgniter 。</li>
+					<li>运行在 CII 框架上的网站响应速度比采用 CodeIgniter 框架提高近10倍。</li>
 				</ul>
 			</div>
 			<div class="recommend-container">
