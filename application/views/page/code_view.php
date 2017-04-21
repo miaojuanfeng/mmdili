1370f3b4bb98ef955c299f90705ba64b36d4e266
USER <user@USERde-iMac.local>
2017-04-21 12:33:54 +0800

    a

--- application/views/page/code_view.php
+++ application/views/page/code_view.php
@@ -21,15 +21,23 @@
 				</ul>
 				<p class="page-title2">Gulp - 前端自动化工具：</p>
 				<ul>
-					<li>gulp-sass，编译sass为css。</li>
-					<li>gulp-minify-css，压缩css。</li>
-					<li>gulp-notify，显示报错信息和报错后不终止当前 gulp 任务。</li>
+					<li>gulp-sass，编译 SASS 为 CSS。</li>
+					<li>gulp-minify-css，压缩 CSS。</li>
+					<li>gulp-notify，显示报错信息和报错后不终止当前 Gulp 任务。</li>
 					<li>gulp-plumber，阻止 gulp 插件发生错误导致进程退出并输出错误日志。</li>
 					<li>gulp-uglify, 压缩 JavaScript。</li>
 					<li>gulp-concat, 合并 JavaScript。</li>
 					<li>gulp-autoprefixer, 增加 CSS 前缀，兼容各浏览器。</li>
 					<li>gulp-rename, 文件重命名。</li>
 					<li>browser-sync，浏览器自动刷新。</li>
+				</ul>
+				<p class="page-title2">特别鸣谢：</p>
+				<ul>
+					<li>阿里云ECS提供高效云计算服务。</li>
+					<li>阿里云OSS提供海量数据存储服务。</li>
+					<li>GoDaddy提供极速域名解析服务。</li>
+					<li>GitHub提供源码托管服务。</li>
+					<li>Microsoft提供文件转码服务。</li>
 				</ul>
 			</div>
 			<div class="recommend-container">
