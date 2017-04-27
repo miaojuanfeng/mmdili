c8dfdf126bff742337e4b300f4dcd9b063c4a553
USER <user@USERde-iMac.local>
2017-04-27 18:41:40 +0800

    a

--- application/controllers/Search.php
+++ application/controllers/Search.php
@@ -28,7 +28,7 @@ class Search extends CI_Controller {
     
 	public function index()
 	{
-		$k = $this->input->get('k')?$this->input->get('k'):'';
+		$k = $this->input->get('k')?mysql_real_escape_string($this->input->get('k')):'';
 		$pn = $this->input->get('pn')?$this->input->get('pn'):1;
 		// 每页显示数量
 		$limit = 20;
