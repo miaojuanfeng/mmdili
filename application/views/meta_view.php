e2f63f991cf1fac5c8ebbbc6273d4494cb94f87a
USER <user@USERde-iMac.local>
2017-04-27 11:59:04 +0800

    a

--- application/views/meta_view.php
+++ application/views/meta_view.php
@@ -1,2 +1,3 @@
 <link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/style.min.css')?>">
 <script type="text/javascript" src="<?=base_url('assets/lib/js/jquery.min.js')?>"></script>
+<link rel="shortcut icon" href="<?=base_url('favicon.ico')?>" type="image/x-icon" />
