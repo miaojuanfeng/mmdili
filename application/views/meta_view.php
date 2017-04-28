9e6652fd5e43fade0b6c37abfa528914b213a5a7
USER <user@USERde-iMac.local>
2017-04-28 16:44:34 +0800

    a

--- application/views/meta_view.php
+++ application/views/meta_view.php
@@ -3,9 +3,3 @@
 <script type="text/javascript" src="<?=base_url('assets/lib/js/jquery.min.js')?>"></script>
 <script type="text/javascript" src="<?=base_url('assets/js/mmdili.js')?>"></script>
 <link rel="shortcut icon" href="<?=base_url('favicon.ico')?>" type="image/x-icon" />
-<?php
-	$browser = get_browser();
-    if(strtolower($browser->browser) == 'safari') {
-        echo '<link href="'.base_url('assets/css/safari.css').'" rel="stylesheet" type="text/css" />';
-    }
-?>
