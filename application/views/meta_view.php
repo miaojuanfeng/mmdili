ecd2c156c2dc252c8d30ec69c04ad2a8394693cd
USER <user@USERde-iMac.local>
2017-04-28 15:57:48 +0800

    a

--- application/views/meta_view.php
+++ application/views/meta_view.php
@@ -1,3 +1,4 @@
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 <link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/mmdili.min.css')?>">
 <script type="text/javascript" src="<?=base_url('assets/lib/js/jquery.min.js')?>"></script>
 <script type="text/javascript" src="<?=base_url('assets/js/mmdili.js')?>"></script>
