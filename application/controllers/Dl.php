8d05708fed4dadb119697ec1e9bf71f154ca115b
miaojuanfeng <miaojuanfeng@qq.com>
2017-05-06 01:30:02 +0800

    upadte secret dl link

--- application/controllers/Dl.php
+++ application/controllers/Dl.php
@@ -45,6 +45,7 @@ class Dl extends CI_Controller {
 		$exist = $this->oss->checkDocExist($object);
     	
 echo $this->oss->getSignedUrlForGettingObject($user_url.'/'.strtotime(date('Y', $doc_url).'-01-01').'/'.$doc_title.'.'.$doc_ext_name);
+die();
     	if($exist){
     		// header('Location:'.'http://doc.mmdili.com/'.$user_url.'/'.strtotime(date('Y', $doc_url).'-01-01').'/'.$doc_title.'.'.$doc_ext_name);
     		$data['dl']['title'] 	= $file['doc_title'];
