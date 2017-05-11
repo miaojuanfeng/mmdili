b6844dee30bbaf49c2b11027216ce1ca7136bd39
USER <user@USERde-iMac.local>
2017-05-11 15:23:03 +0800

    a

--- application/views/user_login_view.php
+++ application/views/user_login_view.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>缪缪文科 - 专业的文科文档分享平台</title>
+	<title>登录_缪缪地理 - 专业的文科文档分享平台</title>
+	<meta name="robots" content="index,follow" />
+	<meta name="keywords" content="高中地理,高中历史,高中政治,高考文综,地理文档,历史文档,政治文档,高考文档,文档下载" />
+	<meta name="description" content="缪缪地理，专业的文科文档分享平台。提供高中地理、历史、政治、文综、高考试题文档下载。" />
 	<?php require_once 'meta_view.php' ?>
 </head>
 <body>
@@ -14,6 +17,7 @@
 				<p class="login-input"><input name="email" type="text" /></p>
 				<p class="login-title2">密码</p>
 				<p class="login-input"><input name="pwd" type="password" /></p>
+				<p class="login-input"><button>登录</button></p>
 			</div>
 			<div class="recommend-container">
 
