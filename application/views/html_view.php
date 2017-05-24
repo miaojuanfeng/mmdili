1661a56a48bfc7007f0fdc17a91c0f4fc8ab4f27
USER <user@USERde-iMac.local>
2017-05-24 16:46:20 +0800

    a

--- application/views/html_view.php
+++ application/views/html_view.php
@@ -69,6 +69,31 @@ pdf2htmlEX.defaultViewer = new pdf2htmlEX.Viewer({});
 					<div id="outline"></div>
 				</div>
 				<div id="page-container">
+					<div class="page-top-adsense">
+						<div class="left">
+							<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+							<!-- view_page_top_left -->
+							<ins class="adsbygoogle"
+							     style="display:inline-block;width:336px;height:280px"
+							     data-ad-client="ca-pub-6393601311110091"
+							     data-ad-slot="2753013484"></ins>
+							<script>
+							(adsbygoogle = window.adsbygoogle || []).push({});
+							</script>
+						</div>
+						<div class="right">
+							<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+							<!-- view_page_top_right -->
+							<ins class="adsbygoogle"
+							     style="display:inline-block;width:336px;height:280px"
+							     data-ad-client="ca-pub-6393601311110091"
+							     data-ad-slot="4229746685"></ins>
+							<script>
+							(adsbygoogle = window.adsbygoogle || []).push({});
+							</script>	
+						</div>
+						<div class="clearfix"></div>
+					</div>
 					<?php
 					for($i=1;$i<=$page['num'];$i++){
 					?>
