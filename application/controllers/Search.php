e450da3e1dc654fb08885ae4a1c8829c2962a772
miaojuanfeng <miaojuanfeng@qq.com>
2017-06-04 15:18:40 +0800

    asdasd

--- application/controllers/Search.php
+++ application/controllers/Search.php
@@ -56,6 +56,10 @@ class Search extends CI_Controller {
 		$cii_pagination['base_url'] = base_url('search?k='.$k.'&pn=');
 		$cii_pagination['per_page'] = $limit;
 		$cii_pagination['total_rows'] = $total;
+		$cii_pagination['first_link'] = '首页';
+		$cii_pagination['prev_link'] = '上一页';
+		$cii_pagination['next_link'] = '下一页';
+		$cii_pagination['last_link'] = '尾页';
 		$this->cii_pagination->initialize($cii_pagination);
 		//
 		$data['class_footer'] = 'fixed-footer';
