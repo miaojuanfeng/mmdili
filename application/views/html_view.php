4df9725c145ae5a0c74f311a0fc08bf68e80fe97
miaojuanfeng <miaojuanfeng@qq.com>
2017-06-18 18:21:56 +0800

    asd

--- application/views/html_view.php
+++ application/views/html_view.php
@@ -14,15 +14,7 @@
 <link rel="stylesheet" href="<?=base_url('assets/lib/css/fancy.min.css')?>"/>
 <link rel="stylesheet" href="http://view.mmdili.com/<?=$page['user_url']?>/<?=$page['doc_url']?>/page.min.css"/>
 <script type="text/javascript" src="<?=base_url('assets/lib/js/htmlview.js')?>"></script>
-<script>
-/*
- Copyright 2012 Mozilla Foundation 
- Copyright 2013 Lu Wang <coolwanglu@gmail.com>
- Apache License Version 2.0 
-*/
-(function(){function b(a,b,e,f){var c=(a.className||"").split(/\s+/g);""===c[0]&&c.shift();var d=c.indexOf(b);0>d&&e&&c.push(b);0<=d&&f&&c.splice(d,1);a.className=c.join(" ");return 0<=d}if(!("classList"in document.createElement("div"))){var e={add:function(a){b(this.element,a,!0,!1)},contains:function(a){return b(this.element,a,!1,!1)},remove:function(a){b(this.element,a,!1,!0)},toggle:function(a){b(this.element,a,!0,!0)}};Object.defineProperty(HTMLElement.prototype,"classList",{get:function(){if(this._classList)return this._classList;
-var a=Object.create(e,{element:{value:this,writable:!1,enumerable:!0}});Object.defineProperty(this,"_classList",{value:a,writable:!1,enumerable:!1});return a},enumerable:!0})}})();
-</script>
+
 <script>
 try{
 pdf2htmlEX.defaultViewer = new pdf2htmlEX.Viewer({});
