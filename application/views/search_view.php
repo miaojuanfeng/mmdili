29c70c59d2c2830bab794f6d3e0cc9342c16e4ef
USER <user@USERde-iMac.local>
2017-07-20 12:20:53 +0800

    asd

--- application/views/search_view.php
+++ application/views/search_view.php
@@ -15,18 +15,6 @@
 				<div class="search-title">
 					<h1>包含 <b><?=$search['key']?></b> <span class="search-title-note">关键字的所有文档</span></h1>
 				</div>
-				<div class="cate-adsense">
-					<!-- <img src="<?=base_url('assets/img/view_ad_2.jpg')?>" width="960" /> -->
-					<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-					<!-- view_page_1 -->
-					<ins class="adsbygoogle"
-					     style="display:inline-block;width:960px;height:90px"
-					     data-ad-client="ca-pub-6393601311110091"
-					     data-ad-slot="3701696288"></ins>
-					<script>
-					(adsbygoogle = window.adsbygoogle || []).push({});
-					</script>
-				</div>
 				<div class="search">
 					<ul>
 						<?php
