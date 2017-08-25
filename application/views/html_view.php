6fd28218d6316cd65b7e29ed39e09276a0e7723d
miaojuanfeng <miaojuanfeng@qq.com>
2017-08-25 19:10:13 +0800

    asd

--- application/views/html_view.php
+++ application/views/html_view.php
@@ -23,7 +23,10 @@
 						<h1><?=$page['title']?></h1>
 					</div>
 					<div class="page-title-notice">
-						<div class="page-info">分类：<a href="#"><?=$page['cate_name']?></a></div>
+						<div class="page-info">馆藏编号：<a href="#"><?=$page['doc_url']?></a></div>
+						<div class="page-info">文档类型：<a href="#"><?=$page['ext_name']?></a></div>
+						<div class="page-info">文档归类：<a href="#"><?=$page['cate_name']?></a></div>
+						<!-- <div class="page-info">发布用户：<a href="#"><?=$page['cate_name']?></a></div> -->
 						<span>提示：已对文档中包含的图片及部分文字进行压缩以加快载入，下载文档可浏览清晰版本。</span>
 						<div class="clearfix"></div>
 					</div>
