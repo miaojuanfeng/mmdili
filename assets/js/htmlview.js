6d30aa30835a6fa3583216791d492a25ef0a4549
miaojuanfeng <miaojuanfeng@qq.com>
2017-08-28 20:16:29 +0800

    asd

--- assets/js/htmlview.js
+++ assets/js/htmlview.js
@@ -1,10 +1,3 @@
 $(document).ready(function(){
-	var wrapper_offset_top = $(".fix-wrapper").offset().top;
-	window.onscroll=function(){
-		if( $(document).scrollTop() >= wrapper_offset_top ){
-			$(".fix-wrapper").addClass('fix-wrapper-fixed');
-		}else{
-			$(".fix-wrapper").removeClass('fix-wrapper-fixed');
-		}
-	}
+	
 });
--- assets/js/mmdili.js
+++ assets/js/mmdili.js
@@ -40,4 +40,13 @@ $(document).ready(function(){
 			$('#search-input').focus();
 		}
 	});
+
+	var wrapper_offset_top = $(".fix-wrapper").offset().top;
+	window.onscroll=function(){
+		if( $(document).scrollTop() >= wrapper_offset_top ){
+			$(".fix-wrapper").addClass('fix-wrapper-fixed');
+		}else{
+			$(".fix-wrapper").removeClass('fix-wrapper-fixed');
+		}
+	}
 });
